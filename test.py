549c56fa1f3580f872b417c4051279c6066ee475
jichengtang <jichengtang@tencent.com>
2022-12-06 12:13:42 +0800

    update for cifar-10

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import torchvision
 from torchvision import datasets
 from torch import nn
 from torch.utils.data import DataLoader
+import time
 
 # Download training data from open datasets.
 training_data = datasets.CIFAR10(
@@ -26,22 +27,27 @@ batch_size = 64
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
+input_shape = None
 for X, y in test_dataloader:
+    input_shape = X.shape
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
 
-model = torchvision.models.resnet50()
+device = "cuda"
+
+model = torchvision.models.resnet50(num_classes=10)
+
+model.to(device)
 
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-device = "cpu"
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     model.train()
     for batch, (X, y) in enumerate(dataloader):
+        start_time = time.time()
         X, y = X.to(device), y.to(device)
 
         # Compute prediction error
@@ -55,7 +61,8 @@ def train(dataloader, model, loss_fn, optimizer):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            cost_time = time.time() - start_time
+            print(f"Train: \n loss: {loss:>7f}  [{current:>5d}/{size:>5d}] cost: {cost_time:>4f}")
 
 
 def test(dataloader, model, loss_fn):
@@ -63,39 +70,37 @@ def test(dataloader, model, loss_fn):
     num_batches = len(dataloader)
     model.eval()
     test_loss, correct = 0, 0
-    input_shape = None
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-            input_shape = X.shape
     test_loss /= num_batches
     correct /= size
     print(
-        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    torch.save(model, "resnet")
+        f"Test: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-    # Export the model
-    dummy_input = torch.tensor(input_shape)
-    torch.onnx.export(model,               # model being run
-                      # model input (or a tuple for multiple inputs)
-                      dummy_input,
-                      # where to save the model (can be a file or file-like object)
-                      "resnet50.onnx",
-                      export_params=True,        # store the trained parameter weights inside the model file
-                      # opset_version=10,          # the ONNX version to export the model to
-                      do_constant_folding=True,  # whether to execute constant folding for optimization
-                      input_names=['input'],   # the model's input names
-                      output_names=['output'],  # the model's output names
-                      dynamic_axes={'input': {0: 'batch_size'},    # variable length axes
-                                    'output': {0: 'batch_size'}})
-
-
-epochs = 5
+epochs = 100
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
     test(test_dataloader, model, loss_fn)
 print("Done!")
+
+model.to("cpu")
+torch.save(model, "resnet50.pt")
+# Export the model
+dummy_input = torch.randn(input_shape)
+torch.onnx.export(model,               # model being run
+                    # model input (or a tuple for multiple inputs)
+                    dummy_input,
+                    # where to save the model (can be a file or file-like object)
+                    "resnet50.onnx",
+                    export_params=True,        # store the trained parameter weights inside the model file
+                    # opset_version=10,          # the ONNX version to export the model to
+                    do_constant_folding=True,  # whether to execute constant folding for optimization
+                    input_names=['input'],   # the model's input names
+                    output_names=['output'],  # the model's output names
+                    dynamic_axes={'input': {0: 'batch_size'},    # variable length axes
+                                'output': {0: 'batch_size'}})
